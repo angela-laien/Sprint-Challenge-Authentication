f60e5b63fd4f7760e2ef4c100ba2b6ee2d84da2f
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-04-24 11:09:59 -0700

    mvp

---
+++ jokes/jokes-router.spec.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+describe('jokes-router', () => {
+    beforeEach(async () => {
+        await db('users').truncate();
+    })
+
+    describe('GET /jokes', () => {
+        it('should return 400 for missing credentials', () => {
+            return request(server)
+                .get('/api/jokes')
+                .then(res => {
+                    expect(res.status).toBe(400)
+                })
+        })
+
+        it('should return JSON', function() {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.type).toMatch(/json/i)
+                })
+        })
+    })
+})
